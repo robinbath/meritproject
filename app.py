8d6635efcd1fa71b3220d9a2b52844d0ade9dc41
Shiying Lin <shiying6120@gmail.com>
2021-01-14 13:43:53 -0500

    add a navigation bar and basic color scheme

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/explore')
+def explore():
+    return render_template("explore.html")
+
+@app.route('/community')
+def community():
+    return render_template("community.html")
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
